bc1ea0991468340b229f1bbf2230e13761b9e3a9
Luiz G. Polido <luiz_polido@outlook.com>
2023-06-20 19:06:14 -0300

    Exercícios em POO

---
+++ src/JavaPOO/Ex001/Program.java
@@ -0,0 +1,37 @@
+package JavaPOO.Ex001;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner teclado = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = teclado.nextLine();
+        System.out.print("Price: ");
+        product.price = teclado.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = teclado.nextInt();
+
+        // System.out.print(product.name + "," + product.price + "," + product.quantity);
+        // Usar o método toString();
+
+        System.out.println("Product Data:" + product);
+
+        System.out.println("\nEnter the number of products to be added in the stock: ");
+        int quantity = teclado.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println("\nProduct Data:" + product);
+
+
+        teclado.close();
+
+    }
+
+}
